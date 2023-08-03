b39263a3e067356c1b2958870b67b1529412c587
Jessica Guo <jessicaguo@email.arizona.edu>
2023-08-03 05:55:40 -0700

    Fix timezone input

--- scripts/02-plot-all.R
+++ scripts/02-plot-all.R
@@ -6,7 +6,8 @@ psy_list <- list()
 
 for(i in 1:length(fn_appended)) {
   ins <- str_remove(fn_appended[i], ".CSV")
-  temp <- read_csv(here::here("data_appended", fn_appended[i])) %>%
+  temp <- read_csv(here::here("data_appended", fn_appended[i]),
+                   locale = locale(tz = "America/Phoenix")) %>%
     mutate(instrument = ins)
   
 
@@ -16,9 +17,11 @@ for(i in 1:length(fn_appended)) {
 psy_df <- do.call(rbind, psy_list)
 
 psy_df %>%
+  # filter(date >= as.Date("2023-07-21")) %>%
   ggplot(aes(x = dt, y = corrected_water_potential_m_pa,
              color = instrument)) +
   geom_point() +
-  facet_wrap(~instrument) +
+  scale_x_datetime(date_labels = "%m-%d") +
+  facet_wrap(~instrument,  ncol = 2) +
   guides(color = "none") +
   theme_bw()
